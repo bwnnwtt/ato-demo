222eee0cf982207cebd4249fda708b5322e4a605
Bennett <puayjianjun@gmail.com>
2023-10-14 20:38:25 +0800

    fix: large layouts styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,34 @@
 const Main = () => {
   return (
     <div className="main">
-      <h1>Member Privleges</h1>
+      <h1>MEMBER PRIVILEGES</h1>
+      <br/>
+      <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem voluptatibus, possimus quaerat soluta sunt animi alias et excepturi impedit explicabo neque maxime assumenda, non dolorum aspernatur consequuntur incidunt quidem repellat?</h2>
+      <br/>
+      <div className="main-item-container">
+        <div className="main-item-img-1">
+          image here
+        </div>
+        <div className="main-item-card-1">
+          card here
+        </div>
+      </div>
+      <div className="main-item-container">
+        <div className="main-item-img-2">
+          image here
+        </div>
+        <div className="main-item-card-2">
+          card here
+        </div>
+      </div>
+      <div className="main-item-container">
+        <div className="main-item-img-3">
+          image here
+        </div>
+        <div className="main-item-card-3">
+          card here
+        </div>
+      </div>
     </div>
   )
 }
